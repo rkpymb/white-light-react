405369419d5ad958a032bef97592c9bbf24f1409
Shivam Srivastava <137923480+yoyoqttt@users.noreply.github.com>
2025-01-14 18:20:48 +0530

    changes

--- src/components/home/HeroSlider.tsx
+++ src/components/home/HeroSlider.tsx
@@ -6,7 +6,7 @@ import banner from "../../../pictures/mainbanner.png"
 const HeroSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const slides = [
-    "../../../pictures/special/slide1.jpg",
+    "/pictures/special/slide1.jpg",
     homedecor,
     banner,
     
